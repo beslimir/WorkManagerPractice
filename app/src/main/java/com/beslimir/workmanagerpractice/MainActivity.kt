68bcb20083a66ea687ca86ffec88b447a6a79a1c
beslimir <marin.beslo@gmail.com>
2022-10-25 19:18:15 +0200

    Initial commit

---
+++ app/src/main/java/com/beslimir/workmanagerpractice/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.beslimir.workmanagerpractice
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.beslimir.workmanagerpractice.ui.theme.WorkManagerPracticeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WorkManagerPracticeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    WorkManagerPracticeTheme {
+        Greeting("Android")
+    }
+}
